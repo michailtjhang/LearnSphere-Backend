aba725a075908dce1c2a83f7a76fc36e5e99c205
MICHAIL <72181056+michailtjhang@users.noreply.github.com>
2025-07-26 10:23:14 +0700

    Adds user registration API with Sanctum-based authentication
    Introduces a new AccountController with a user registration endpoint, utilizing Laravel validation and hashed passwords for security.
    
    Integrates Laravel Sanctum for API token authentication by adding necessary configuration, middleware, and routes.
    
    Includes migration for personal access tokens and updates the User model to support Sanctum. Updates composer dependencies to include Sanctum.

---
+++ app/Http/Controllers/front/AccountController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\front;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AccountController extends Controller
+{
+    public function register(Request $request)
+    {
+        
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255|min:5',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        // This will return validation errors if any
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => '400',
+                'errors' => $validator->errors(),
+            ], 400);
+        }
+
+        // Now save user info in database
+        $user = new User();
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = Hash::make($request->input('password'));
+        $user->save();
+
+        return response()->json([
+            'status' => '200',
+            'message' => 'User registered successfully',
+        ], 200);
+    }
+}
