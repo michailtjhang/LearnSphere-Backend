7ccde9b69ee8efff48352837e04b89dafeea3fd7
MICHAIL <72181056+michailtjhang@users.noreply.github.com>
2025-08-03 15:01:44 +0700

    Add user authentication method to AccountController and update API routes

--- app/Http/Controllers/front/AccountController.php
+++ app/Http/Controllers/front/AccountController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\front;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -12,7 +13,7 @@ class AccountController extends Controller
 {
     public function register(Request $request)
     {
-        
+        // Validate incoming request
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255|min:5',
             'email' => 'required|string|email|max:255|unique:users',
@@ -38,5 +39,42 @@ class AccountController extends Controller
             'status' => '200',
             'message' => 'User registered successfully',
         ], 200);
+    }
+
+    public function authenticate(Request $request)
+    {
+        // Validate incoming request
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email',
+            'password' => 'required|string|min:8',
+        ]);
+
+        // This will return validation errors if any
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => '400',
+                'errors' => $validator->errors(),
+            ], 400);
+        }
+
+        if (Auth::attempt(['email' => $request->input('email'), 'password' => $request->input('password')])) {
+            // Authentication passed...
+            // Generate a token for the user
+            $user = User::find(Auth::user()->id);
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'status' => '200',
+                'token' => $token,
+                'name' => $user->name,
+                'id' => Auth::user()->id,
+                'message' => 'User authenticated successfully',
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => '401',
+                'message' => 'Either email or password is incorrect',
+            ], 401);
+        }
     }
 }
